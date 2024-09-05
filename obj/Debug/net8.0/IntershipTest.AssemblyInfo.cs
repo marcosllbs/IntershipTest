4f0be85469caa3d548ed6cdfdc725f21453367cc
Marcos Lucas <marcosllbs@hotmail.com>
2024-09-05 12:36:38 -0300

    Initial commit

--- obj/Debug/net8.0/IntershipTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/IntershipTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntershipTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de4d75b3b114d28bb0991cb78fa6718209c6b33")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntershipTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntershipTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
